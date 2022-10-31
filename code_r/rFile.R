f7a1ebd7b62b39fd3aee286b8ce6adbe6579a1a3
EdgarTip <edgartip2003@gmail.com>
2022-10-31 03:12:56 +0000

    IT WORKSSSSSSSSSSSSSSSS

--- code_r/rFile.R
+++ code_r/rFile.R
@@ -1,33 +1,40 @@
+
 library(abind)
 library(ggplot2)
+library(ggpubr)
+library(ggpmisc)
 
 filename = "../data/resAll.csv"
+
+
+
+
+
 
 dataset = read.csv(filename, header = TRUE, sep = ",", dec = ".")
 
 step = 10
 amount_algorithms = 3
 
-#print(dataset)
 
 global_array <- array(numeric(),c(step,amount_algorithms,0)) 
 
 for(i in seq(from = 0, to = nrow(dataset)-1, by = amount_algorithms)){
-    test_array = array(numeric(),c(step,0))    
+  test_array = array(numeric(),c(step,0))    
   
-    for(k in 1:amount_algorithms){
-
-      algorithm_array <- rep(NA,step)
+  for(k in 1:amount_algorithms){
+    
+    algorithm_array <- rep(NA,step)
+    
+    for(j in 1:step){
       
-      for(j in 1:step){
-
-        algorithm_array[j] = dataset[i+k,j]
-      }
-      
-      test_array <- abind(test_array,algorithm_array)
+      algorithm_array[j] = dataset[i+k,j]
     }
     
-    global_array <- abind(global_array,test_array)
+    test_array <- abind(test_array,algorithm_array)
+  }
+  
+  global_array <- abind(global_array,test_array)
 }
 
 #print(global_array)
@@ -37,9 +44,9 @@ for(i in seq(from = 0, to = nrow(dataset)-1, by = amount_algorithms)){
 #Type 2 = n_vertices a mudar
 #Type 3 = p_value a mudar
 type = 2; 
-n_vertices = 500;
+n_vertices = 2100;
 p_value = 0.37;
-capacity = 210;
+capacity = 340;
 
 n_vertices_array_size = 7 
 p_array_size = 8
@@ -53,27 +60,25 @@ amount = 0;
 
 
 if(type == 1){
+  amount = capacity_array_size
   x = c(10, 20, 30, 50, 80, 130, 210, 340, 500)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))
   dinic_array  <- array(numeric(),c(number_test,0))   
   
 }else if(type == 2){
+  amount = n_vertices_array_size
   x = c(100, 200, 300, 500, 800, 1300, 2100)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))   
   dinic_array  <- array(numeric(),c(number_test,0))   
 }else{
+  amount = p_array_size
   x = c(0.1, 0.15, 0.2, 0.25, 0.37, 0.5, 0.75, 1)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))   
   dinic_array  <- array(numeric(),c(number_test,0))   
 }
-
-
-i_mpm = 1
-i_ek = 1
-i_dinic = 1
 
 
 
@@ -84,19 +89,13 @@ if(type == 1){
       if(global_array[3,j,i] == p_value && global_array[4,j,i] == n_vertices){
         
         if(global_array[2,j,i] == "MPM"){
-          aux <-global_array[6:10,j,i]
-          mpm_array <- abind(mpm_array,aux) 
-          #i_mpm <- i_mpm + 1
+          mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i]))
         }
         else if(global_array[2,j,i] == "EK"){
-          print(global_array[6:10,j,i])
-          aux <-global_array[6:10,j,i]
-          ek_array <- abind(ek_array,aux) 
-          #i_ek <- i_ek + 1
+          ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
         }
         else{
-          dinic_array <- abind(dinic_array,global_array[6:10,j,i]) 
-          #i_dinic <- i_dinic + 1
+          dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
         }
       }
     }
@@ -106,18 +105,15 @@ if(type == 1){
   for(i in 1:arr_dim[3]){
     for(j in 1:arr_dim[2]){
       if(global_array[3,j,i] == p_value && global_array[5,j,i] == capacity){
-
+        
         if(global_array[2,j,i] == "MPM"){
-          mpm_array <- abind(mpm_array,global_array[6:10,j,i]) 
-          #i_mpm <- i_mpm + 1
+          mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i])) 
         }
         else if(global_array[2,j,i] == "EK"){
-          ek_array <- abind(ek_array,global_array[6:10,j,i]) 
-          #i_ek <- i_ek + 1
+          ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
         }
         else{
-          dinic_array <- abind(dinic_array,global_array[6:10,j,i]) 
-          #i_dinic <- i_dinic + 1
+          dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
         }
       }
     }
@@ -128,15 +124,15 @@ if(type == 1){
     for(j in 1:arr_dim[2]){
       if(global_array[4,j,i] == n_vertices && global_array[5,j,i] == capacity){
         if(global_array[2,j,i] == "MPM"){
-          mpm_array <- abind(mpm_array,global_array[6:10,j,i]) 
+          mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i]))
           #i_mpm <- i_mpm + 1
         }
         else if(global_array[2,j,i] == "EK"){
-          ek_array <- abind(ek_array,global_array[6:10,j,i]) 
+          ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
           #i_ek <- i_ek + 1
         }
         else{
-          dinic_array <- abind(dinic_array,global_array[6:10,j,i]) 
+          dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
           #i_dinic <- i_dinic + 1
         }
       }
@@ -146,22 +142,61 @@ if(type == 1){
 }
 
 
-
-
-
-
 print(dinic_array)
 print(mpm_array)
 print(ek_array)
 
 
+dinic_avg = colMeans(dinic_array)
+mpm_avg = colMeans(mpm_array)
+ek_avg = colMeans(ek_array)
+
 
 #Scatter Plot
-b = data.frame(dinic_array)
-print(b)
+print(c(dinic_avg,mpm_avg, ek_avg))
 
-p <- ggplot(dsmall, aes(carat, price, color=color)) + geom_point(size=4)
 
+df = data.frame(x,c(dinic_avg,mpm_avg, ek_avg))
+colnames(df) <- c('x','y')
+df$group <- factor(c(rep(c("Dinic"), amount),rep(c("Mpm"), amount),rep(c("Ek"), amount )))
+
+print(df)
+
+#m <- lm(y ~ x, b);
+
+p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
+  stat_poly_line(formula = y ~ poly(x, 2, raw = TRUE)) +
+  stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
+               aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "*\", \"*")))+
+  
+  geom_point() + ylim(-5,50)+
+  labs(title="Fixing vertices",
+                                x ="number vertices", y = "Time(s)")
+
+print(p)
+
+#ggplot(data = df, aes(x = x, y = y2))
+#  stat_poly_line(formula = y ~poly(x, 2, raw = TRUE)) +
+#  stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
+#               aes(label = after_stat(eq.label)))
+
+
+#ggplot(data = df, aes(x = x, y = y2))
+#  stat_poly_line(formula = y ~poly(x, 2, raw = TRUE)) +
+#  stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
+#                 aes(label = after_stat(eq.label)))+
+#  geom_point()
+
+
+
+#p <- ggplot(b) +geom_jitter(aes(x,dinic_avg),colour="red")+
+#  geom_smooth(method = "lm" ,aes(x,dinic_avg,col="dinic"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1) +
+
+#geom_jitter(aes(x,ek_avg),colour="green")+geom_smooth(method = "lm" ,aes(x,ek_avg,col="ek"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1) +
+#  geom_jitter(aes(x,mpm_avg),colour="blue")+geom_smooth(method = "lm" ,aes(x,mpm_avg,col="mpm"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1)
+
+#p1 <- p +geom_text(x=500,y = 0.6, label= lm_eqn(b), parse = TRUE)
+#print(p1)
 
 
 #Q-q plot
