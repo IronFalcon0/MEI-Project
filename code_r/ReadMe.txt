94235e31162feb5590f4780df01652637bf2cb27
EdgarTip <edgartip2003@gmail.com>
2022-11-01 03:24:28 +0000

    stuff

---
+++ code_r/ReadMe.txt
@@ -0,0 +1,41 @@
+52- type -> importante. Define que tipo de teste estamos a fazer. 
+	A-Para scatter plots :
+		1= n_capacidade a variar
+		2= n_vertices a variar
+		3= p_value a variar
+		
+	B-Para 3d plots:
+		1= analisar impacto da dupla(vertices, p)
+		2= analisar impacto da dupla(capacidade, p)
+		3= analisar impacto da dupla(vertices, capacidade)
+
+53- n_vertices = valor de vertices a fixar
+54- p_value = valor p a fixar
+55- capacity = valor de capacidade a fixar
+
+53,54,55 só são necessarios em gráficos em que o seu valor tem de ser fixo assim são necessarios para:
+	A1 - 53, 54
+	A2 - 54, 55
+	A3 - 53, 55
+	B1 - 55
+	B2 - 53
+	B3 - 54
+(sao 3 da manha posso me ter baralhado mas acho que é isto xd)
+
+57,58,59 sao os valores do tamanho de casos teste de cada. Descomentar se estiver comentado e apagar os valores nao comentados. Se adicionarem testes (tipo adicionam experiencias com o valor de 520 vertices) incrementem isto senao da merda nos scatter plots.
+
+75,87,98 descomentem essas linhas e apaguem o outro x = (se usarem o novo dataset)
+
+66- numero de vezes que uma experiencia corre deixar a 5
+68- nao mexer
+
+nos plots podem alterar definicoes tranquilamente. 
+
+TEM DE DAR FIX NO FACTO DE O EK MANDAR VALORES A 0. O RODAS PROVAVELMENTE VAI TER DE CORRER ALGUNS TESTES ESPECIFICOS
+PARA O EK ATE ELE SAIR DA TELA NOS SCATTER PLOTS. NOS GRAFICOS 3D N SEI COMO VAO FAZER TALVEZ CORTEM SÓ TODOS OS VALORES = 0 ISSO DEVE TAR ALGURES NA NET COMO FAZER.
+
+
+
+
+
+-Para ativarem o scatter plots metam showScatter (185) a TRUE. Msm para os 3D plots
--- code_r/rFile.R
+++ code_r/rFile.R
@@ -49,10 +49,14 @@ for(i in seq(from = 0, to = nrow(dataset)-1, by = amount_algorithms)){
 #Type 1 = n_capacidade a mudar
 #Type 2 = n_vertices a mudar
 #Type 3 = p_value a mudar
-type = 1; 
-n_vertices = 2100;
+type = 3; 
+n_vertices = 1300;
 p_value = 0.37;
-capacity = 340;
+capacity = 210;
+
+#n_vertices_array_size = 27 
+#p_array_size = 14
+#capacity_array_size = 9 
 
 n_vertices_array_size = 7 
 p_array_size = 8
@@ -63,11 +67,12 @@ number_test = 5
 arr_dim = dim(global_array)
 amount = 0;
 
-plot_type = "EK"
+plot_type = "Dinic"
 
 if(type == 1){
   amount = capacity_array_size
   string = "capacity"
+  #x = c(10, 20, 30, 50, 80, 130, 210, 340, 500)
   x = c(10, 20, 30, 50, 80, 130, 210, 340, 500)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))
@@ -79,23 +84,25 @@ if(type == 1){
 }else if(type == 2){
   string = "number vertices"
   amount = n_vertices_array_size
+  #x = c(100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2300, 2600, 2900, 3200, 3500, 4000, 4500, 5000, 5500)
   x = c(100, 200, 300, 500, 800, 1300, 2100)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))   
   dinic_array  <- array(numeric(),c(number_test,0)) 
-  values_capacity = rep(NA,n_vertices_array_size*capacity_array_size)
-  values_p = rep(NA,n_vertices_array_size*capacity_array_size)
-  times = rep(NA,n_vertices_array_size*p_array_size)
+  values_capacity = c()
+  values_p = c()
+  times = c()
 }else{
   string = "p_value"
   amount = p_array_size
+  #x = c(0.12, 0.15, 0.2, 0.25, 0.30, 0.35, 0.4, 0.5, 0.6, 0.8, 1)
   x = c(0.1, 0.15, 0.2, 0.25, 0.37, 0.5, 0.75, 1)
   mpm_array <- array(numeric(),c(number_test,0))   
   ek_array  <- array(numeric(),c(number_test,0))   
   dinic_array  <- array(numeric(),c(number_test,0))   
   values_vertices = c()
-  values_capacity = rep(NA,n_vertices_array_size*capacity_array_size)
-  times = rep(NA,n_vertices_array_size*p_array_size)
+  values_capacity = c()
+  times = c()
 }
 
 
@@ -107,7 +114,6 @@ if(type == 1){
       if(global_array[5,j,i] == capacity && plot_type ==global_array[2,j,i] ){
         values_vertices <- c(values_vertices,rep( as.numeric(global_array[4,j,i]),number_test))
         values_p <- c(values_p,rep( as.numeric(global_array[3,j,i]),number_test))
-        print(as.numeric(global_array[6:10,j,i]))
         times <- c(times,  as.numeric(global_array[6:10,j,i]))
 
         
@@ -130,6 +136,13 @@ if(type == 1){
 }else if(type == 2){
   for(i in 1:arr_dim[3]){
     for(j in 1:arr_dim[2]){
+      
+      if(global_array[4,j,i] == n_vertices && plot_type ==global_array[2,j,i] ){
+        values_capacity <- c(values_capacity,rep( as.numeric(global_array[5,j,i]),number_test))
+        values_p <- c(values_p,rep( as.numeric(global_array[3,j,i]),number_test))
+        times <- c(times,  as.numeric(global_array[6:10,j,i]))
+      }
+      
       if(global_array[3,j,i] == p_value && global_array[5,j,i] == capacity){
         
         if(global_array[2,j,i] == "MPM"){
@@ -148,18 +161,22 @@ if(type == 1){
 }else if(type == 3){
   for(i in 1:arr_dim[3]){
     for(j in 1:arr_dim[2]){
+      
+      if(global_array[3,j,i] == p_value && plot_type ==global_array[2,j,i] ){
+        values_capacity <- c(values_capacity,rep( as.numeric(global_array[5,j,i]),number_test))
+        values_vertices <- c(values_vertices,rep( as.numeric(global_array[4,j,i]),number_test))
+        times <- c(times,  as.numeric(global_array[6:10,j,i]))
+      }
+      
       if(global_array[4,j,i] == n_vertices && global_array[5,j,i] == capacity){
         if(global_array[2,j,i] == "MPM"){
           mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i]))
-          #i_mpm <- i_mpm + 1
         }
         else if(global_array[2,j,i] == "EK"){
           ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
-          #i_ek <- i_ek + 1
         }
         else{
           dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
-          #i_dinic <- i_dinic + 1
         }
       }
     }
@@ -172,9 +189,9 @@ print(dinic_array)
 print(mpm_array)
 print(ek_array)
 
-showScatter = FALSE
+showScatter = TRUE
 if(showScatter){
-
+  
   dinic_avg = colMeans(dinic_array)
   mpm_avg = colMeans(mpm_array)
   ek_avg = colMeans(ek_array)
@@ -183,23 +200,27 @@ if(showScatter){
   #Scatter Plot
   print(c(dinic_avg,mpm_avg, ek_avg))
   
-  
+  print(x)
   df = data.frame(x,c(dinic_avg,mpm_avg, ek_avg))
+  #df = data.frame(x,c(dinic_avg,mpm_avg))
   colnames(df) <- c('x','y')
   df$group <- factor(c(rep(c("Dinic"), amount),rep(c("Mpm"), amount),rep(c("Ek"), amount )))
+  #df$group <- factor(c(rep(c("Dinic"), amount),rep(c("Mpm"), amount)))
+  #type = 1
   print(df)
-  yub = 20
-  ylb = -1
+  
   
   #m <- lm(y ~ x, b);
   if(type == 1){
+    yub = 1
+    ylb = 0
     p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
       stat_poly_line(formula = y ~ poly(x, 1, raw = TRUE)) +
       stat_poly_eq(formula = y ~ poly(x, 1, raw = TRUE),
                    aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "*\", \"*")))+
       
       geom_point() + ylim(ylb,yub)+
-      labs(title="Fixing vertices",
+      labs(title="Capacity Evolution",
            x = string, y = "Time(s)")
     
     print(p)
@@ -207,14 +228,15 @@ if(showScatter){
   }
   
   if(type == 2 || type == 3){
-  
+    yub = 0.1
+    ylb = 0
   p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
     stat_poly_line(formula = y ~ poly(x, 2, raw = TRUE)) +
     stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
                  aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "*\", \"*")))+
     
     geom_point() + ylim(ylb,yub)+
-    labs(title="Fixing vertices",
+    labs(title="Number Vertices Evolution",
                                   x = string, y = "Time(s)")
   
   print(p)
@@ -223,82 +245,42 @@ if(showScatter){
 
 #3D graph
 
-
-if(type == 1){
-  print(values_p)
-  df = data.frame(values_p,values_vertices, times)
+show3D = FALSE
+if(show3D){
+  if(type == 1){
+    df = data.frame(values_p,values_vertices, times)
   
-  #print(df)
-  #colors <- c("royalblue1","royalblue1")
-  #df$color <- colors[ as.numeric( as.factor(iris$Species) ) ]
-  #print(df)
-  #plot3d( df[,1], df[,2], df[,3], col = iris$color, type = "s", radius = .2 )
-  print(df)
-  
-  x <- df$values_p
-  y <- df$values_vertices
-  z <- df$times
-  
-  print(x)
-  scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "Probability",
-            ylab ="Number Vertices", zlab = "time", clab = c("Time(s)")
-            ,ticktype = "detailed")
-  
+    x <- df$values_p
+    y <- df$values_vertices
+    z <- df$times
+    
+    scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "Probability",
+              ylab ="Number Vertices", zlab = "time", clab = c("Time(s)")
+              ,ticktype = "detailed")
+    
+  }
+  if(type == 2){
+    print(values_p)
+    df = data.frame(values_p,values_capacity, times)
+    print(df)
+    x <- df$values_p
+    y <- df$values_capacity
+    z <- df$times
+    
+    scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "Probability",
+              ylab ="Capacity", zlab = "time", clab = c("Time(s)")
+              ,ticktype = "detailed")
+  }
+  if(type == 3){
+    df = data.frame(values_capacity,values_vertices, times)
+    
+    x <- df$values_capacity
+    y <- df$values_vertices
+    z <- df$times
+    
+    scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "capacity",
+              ylab ="vertices", zlab = "time", clab = c("Time(s)")
+              ,ticktype = "detailed")
+    
+  }
 }
-
-#ggplot(data = df, aes(x = x, y = y2))
-#  stat_poly_line(formula = y ~poly(x, 2, raw = TRUE)) +
-#  stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
-#               aes(label = after_stat(eq.label)))
-
-
-#ggplot(data = df, aes(x = x, y = y2))
-#  stat_poly_line(formula = y ~poly(x, 2, raw = TRUE)) +
-#  stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
-#                 aes(label = after_stat(eq.label)))+
-#  geom_point()
-
-
-
-#p <- ggplot(b) +geom_jitter(aes(x,dinic_avg),colour="red")+
-#  geom_smooth(method = "lm" ,aes(x,dinic_avg,col="dinic"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1) +
-
-#geom_jitter(aes(x,ek_avg),colour="green")+geom_smooth(method = "lm" ,aes(x,ek_avg,col="ek"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1) +
-#  geom_jitter(aes(x,mpm_avg),colour="blue")+geom_smooth(method = "lm" ,aes(x,mpm_avg,col="mpm"), formula= (y ~ x + I(x^2)), se=FALSE, linetype = 1)
-
-#p1 <- p +geom_text(x=500,y = 0.6, label= lm_eqn(b), parse = TRUE)
-#print(p1)
-
-
-#Q-q plot
-
-
-
-#Bar plot with error bars
-
-
-
-
-#Density Plot with Area Coloring??
-
-
-#p <- ggplot(b, aes(color, /carat, fill=color)) + geom_boxplot()
-#print(p)
-
-
-#print(c[3])
-
-#run = c(1,2,3,4,5)
-
-
-#global_array[3:dim(global_array)[1],1,1] = c(1,2,3,4,5)
-#b = data.frame(global_array[3:dim(global_array)[1],1,1])
-
-#print(b)
-#model <- lm(y~x, data=b)
-#summary(model)
-#g <- ggplot(b, aes(c(1,2,3,4,5),X1)) + geom_point() + geom_smooth(method="lm") + theme(panel.background=element_rect(fill = "white", colour = "black"))
-#print(g)
-#for (i in 1:dim(global_array)[3]) {
-#  
-#}
