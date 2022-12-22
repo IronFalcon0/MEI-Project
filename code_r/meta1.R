f94a1a4f0788ea2476878126a393d4715fd2839f
EdgarTip <edgartip2003@gmail.com>
2022-12-22 01:27:05 +0000

    save point

---
+++ code_r/meta1.R
@@ -0,0 +1,352 @@
+
+meta1 <- function(filename){
+  ignore_ek_after = 2000
+  
+  
+  dataset = read.csv(filename, header = TRUE, sep = ",", dec = ".")
+  
+  step = 10
+  amount_algorithms = 3
+  
+  
+  global_array <- array(numeric(),c(step,amount_algorithms,0)) 
+  
+  for(i in seq(from = 0, to = nrow(dataset)-1, by = amount_algorithms)){
+    test_array = array(numeric(),c(step,0))    
+    
+    for(k in 1:amount_algorithms){
+      
+      algorithm_array <- rep(NA,step)
+      
+      for(j in 1:step){
+        
+        algorithm_array[j] = dataset[i+k,j]
+      }
+      if (algorithm_array[2] == "EK" && as.numeric(algorithm_array[4]) > ignore_ek_after) {
+        algorithm_array[6] <- "1000"
+        algorithm_array[7] <- "1000"
+        algorithm_array[8] <- "1000"
+        algorithm_array[9] <- "1000"
+        algorithm_array[10] <- "1000"
+      }
+      
+      test_array <- abind(test_array,algorithm_array)
+    }
+    
+    global_array <- abind(global_array,test_array)
+  }
+  
+  #print(global_array)
+  
+  
+  #Type 1 = n_capacidade a mudar
+  #Type 2 = n_vertices a mudar
+  #Type 3 = p_value a mudar
+  type = 3; 
+  n_vertices = 400;
+  p_value = 0.8;
+  capacity = 340;
+  show3D = FALSE
+  showScatter = TRUE
+  plot_type = "EK" # 1) EK 2) MPM 3) Dinic (atenção aos upper e lower cases)
+  n_vertices_cap = 2000 # discards info of graphs larger than n, only applies for type 1 and 3
+  time_cap = 0.13
+  
+  p_array_size = 11
+  n_vertices_array_size = 27 
+  capacity_array_size = 9 
+  
+  # n_vertices_array_size = 7 
+  # p_array_size = 8
+  # capacity_array_size = 9 
+  
+  
+  number_test = 5
+  arr_dim = dim(global_array)
+  amount = 0;
+  
+  if(type == 1){
+    amount = capacity_array_size
+    string = "capacity"
+    
+    x = c(10, 20, 30, 50, 80, 130, 210, 340, 500)
+    mpm_array <- array(numeric(),c(number_test,0))   
+    ek_array  <- array(numeric(),c(number_test,0))
+    dinic_array  <- array(numeric(),c(number_test,0))   
+    values_vertices = c()
+    values_p = c()
+    times = c()
+    
+  }else if(type == 2){
+    string = "number vertices"
+    
+    
+    temp = c(100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2300, 2600, 2900, 3200, 3500, 4000, 4500, 5000, 5500)
+    x = c()
+    for(id in 1:length(temp)) {
+      if (temp[id] > n_vertices_cap) {
+        break
+      }
+      x <- c(x, temp[id])
+    }
+    
+    amount = length(x)
+    
+    mpm_array <- array(numeric(),c(number_test,0))   
+    ek_array  <- array(numeric(),c(number_test,0))   
+    dinic_array  <- array(numeric(),c(number_test,0)) 
+    values_capacity = c()
+    values_p = c()
+    times = c()
+  }else{
+    string = "p_value"
+    amount = p_array_size
+    
+    x = c(0.12, 0.15, 0.2, 0.25, 0.30, 0.35, 0.4, 0.5, 0.6, 0.8, 1)
+    
+    mpm_array <- array(numeric(),c(number_test,0))   
+    ek_array  <- array(numeric(),c(number_test,0))   
+    dinic_array  <- array(numeric(),c(number_test,0))   
+    values_vertices = c()
+    values_capacity = c()
+    times = c()
+  }
+  
+  
+  
+  #Capacidade fixo
+  if(type == 1){
+    for(i in 1:arr_dim[3]){
+      for(j in 1:arr_dim[2]){
+        if(as.numeric(global_array[4,j,i]) > n_vertices_cap) {
+          next
+        }
+        
+        if(global_array[5,j,i] == capacity && plot_type ==global_array[2,j,i] ){
+          values_vertices <- c(values_vertices,rep( as.numeric(global_array[4,j,i]),number_test))
+          values_p <- c(values_p,rep( as.numeric(global_array[3,j,i]),number_test))
+          times <- c(times,  as.numeric(global_array[6:10,j,i]))
+          
+        }
+        if(global_array[3,j,i] == p_value && global_array[4,j,i] == n_vertices){
+          
+          if(global_array[2,j,i] == "MPM"){
+            mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i]))
+          }
+          else if(global_array[2,j,i] == "EK"){
+            ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
+          }
+          else{
+            dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
+          }
+        }
+      }
+      
+    }
+  }else if(type == 2){
+    for(i in 1:arr_dim[3]){
+      for(j in 1:arr_dim[2]){
+        if(global_array[4,j,i] != n_vertices && as.numeric(global_array[4,j,i]) > n_vertices_cap) {
+          next
+        }
+        
+        if(global_array[4,j,i] == n_vertices && plot_type ==global_array[2,j,i] ){
+          values_capacity <- c(values_capacity,rep( as.numeric(global_array[5,j,i]),number_test))
+          values_p <- c(values_p,rep( as.numeric(global_array[3,j,i]),number_test))
+          times <- c(times,  as.numeric(global_array[6:10,j,i]))
+        }
+        
+        if(global_array[3,j,i] == p_value && global_array[5,j,i] == capacity){
+          
+          if(global_array[2,j,i] == "MPM"){
+            mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i])) 
+          }
+          else if(global_array[2,j,i] == "EK"){
+            ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
+          }
+          else{
+            dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
+          }
+        }
+      }
+      
+    }
+  }else if(type == 3){
+    for(i in 1:arr_dim[3]){
+      for(j in 1:arr_dim[2]){
+        if(as.numeric(global_array[4,j,i]) > n_vertices_cap) {
+          next
+        }
+        
+        if(global_array[3,j,i] == p_value && plot_type ==global_array[2,j,i] ){
+          values_capacity <- c(values_capacity,rep( as.numeric(global_array[5,j,i]),number_test))
+          values_vertices <- c(values_vertices,rep( as.numeric(global_array[4,j,i]),number_test))
+          times <- c(times,  as.numeric(global_array[6:10,j,i]))
+        }
+        
+        if(global_array[4,j,i] == n_vertices && global_array[5,j,i] == capacity){
+          if(global_array[2,j,i] == "MPM"){
+            mpm_array <- abind(mpm_array,as.numeric(global_array[6:10,j,i]))
+          }
+          else if(global_array[2,j,i] == "EK"){
+            ek_array <- abind(ek_array,as.numeric(global_array[6:10,j,i]))
+          }
+          else{
+            dinic_array <- abind(dinic_array,as.numeric(global_array[6:10,j,i]))
+          }
+        }
+      }
+      
+    }
+  }
+  
+  
+  print(dinic_array)
+  print(mpm_array)
+  print(ek_array)
+  
+  
+  if(showScatter){
+    
+    dinic_avg = colMeans(dinic_array)
+    mpm_avg = colMeans(mpm_array)
+    ek_avg = colMeans(ek_array)
+    
+    
+    #Scatter Plot
+    print(c(dinic_avg,mpm_avg, ek_avg))
+    
+    print(x)
+    df = data.frame(x,c(dinic_avg,mpm_avg, ek_avg))
+    #df = data.frame(x,c(dinic_avg,mpm_avg))
+    colnames(df) <- c('x','y')
+    df$group <- factor(c(rep(c("Dinic"), amount),rep(c("Mpm"), amount),rep(c("Ek"), amount )))
+    #df$group <- factor(c(rep(c("Dinic"), amount),rep(c("Mpm"), amount)))
+    #type = 1
+    print(df)
+    
+    
+    #m <- lm(y ~ x, b);
+    if(type == 1){
+      yub = max(c(max(mpm_avg), max(dinic_avg), max(ek_avg))) * 1.25 # deixar espaço em cima do gráfico
+      yub = min(yub, time_cap)
+      ylb = 0
+      p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
+        stat_poly_line(formula = y ~ poly(x, 1, raw = TRUE)) +
+        stat_poly_eq(formula = y ~ poly(x, 1, raw = TRUE),
+                     aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "*\", \"*")))+
+        
+        geom_point() + ylim(ylb,yub)+
+        labs(title="Capacity Evolution",
+             x = string, y = "Time(s)")
+      
+      print(p)
+      
+    }
+    
+    if(type == 2 || type == 3){
+      if(type == 2) {
+        str_title = "Number Vertices Evolution"
+      } else {
+        str_title = "Connection probability Evolution"
+      }
+      yub = max(c(max(mpm_avg), max(dinic_avg), max(ek_avg))) * 1.25 # deixar espaço em cima do gráfico
+      yub = min(yub, time_cap)
+      ylb = 0
+      p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
+        stat_poly_line(formula = y ~ poly(x, 2, raw = TRUE)) +
+        stat_poly_eq(formula = y ~ poly(x, 2, raw = TRUE),
+                     aes(label = paste(after_stat(eq.label), after_stat(rr.label), sep = "*\", \"*")))+
+        
+        geom_point() + ylim(ylb,yub)+
+        labs(title=str_title,
+             x = string, y = "Time(s)")
+      
+      print(p)
+    }
+  }
+  
+  #3D graph
+  
+  
+  if(show3D){
+    if(type == 1){
+      df = data.frame(values_p,values_vertices, times)
+      
+      print(times)
+      
+      x <- df$values_p
+      y <- df$values_vertices
+      z <- df$times
+      
+      #ONLY GOD KNOWS HOW THIS WORKS!!!!!!!!!!!!!
+      fit <- lm(z ~ x + y)
+      # predict values on regular xy grid
+      grid.lines = 26
+      x.pred <- seq(min(x), max(x), length.out = grid.lines)
+      y.pred <- seq(min(y), max(y), length.out = grid.lines)
+      xy <- expand.grid( x = x.pred, y = y.pred)
+      z.pred <- matrix(predict(fit, newdata = xy), 
+                       nrow = grid.lines, ncol = grid.lines)
+      
+      fitpoints <- predict(fit)
+      
+      scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "Probability",
+                ylab ="Number Vertices", zlab = "time", clab = c("Time(s)")
+                ,ticktype = "detailed", surf = list(x = x.pred, y = y.pred, z = z.pred,  
+                                                    facets = NA, fit = fitpoints))
+      
+    }
+    if(type == 2){
+      print(values_p)
+      df = data.frame(values_p,values_capacity, times)
+      print(df)
+      x <- df$values_p
+      y <- df$values_capacity
+      z <- df$times
+      
+      
+      #ONLY GOD KNOWS HOW THIS WORKS!!!!!!!!!!!!!
+      fit <- lm(z ~ x + y)
+      # predict values on regular xy grid
+      grid.lines = 26
+      x.pred <- seq(min(x), max(x), length.out = grid.lines)
+      y.pred <- seq(min(y), max(y), length.out = grid.lines)
+      xy <- expand.grid( x = x.pred, y = y.pred)
+      z.pred <- matrix(predict(fit, newdata = xy), 
+                       nrow = grid.lines, ncol = grid.lines)
+      
+      fitpoints <- predict(fit)
+      scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "Probability",
+                ylab ="Capacity", zlab = "time", clab = c("Time(s)")
+                ,ticktype = "detailed", surf = list(x = x.pred, y = y.pred, z = z.pred,  
+                                                    facets = NA, fit = fitpoints))
+    }
+    if(type == 3){
+      df = data.frame(values_capacity,values_vertices, times)
+      
+      x <- df$values_capacity
+      y <- df$values_vertices
+      z <- df$times
+      
+      
+      #ONLY GOD KNOWS HOW THIS WORKS!!!!!!!!!!!!!
+      fit <- lm(z ~ x + y)
+      # predict values on regular xy grid
+      grid.lines = 26
+      x.pred <- seq(min(x), max(x), length.out = grid.lines)
+      y.pred <- seq(min(y), max(y), length.out = grid.lines)
+      xy <- expand.grid( x = x.pred, y = y.pred)
+      z.pred <- matrix(predict(fit, newdata = xy), 
+                       nrow = grid.lines, ncol = grid.lines)
+      
+      fitpoints <- predict(fit)
+      
+      scatter3D(x,y,z, pch = 16, theta = 50,phi = 0, bty = "g",main = paste(plot_type," data"), xlab = "capacity",
+                ylab ="vertices", zlab = "time", clab = c("Time(s)")
+                ,ticktype = "detailed", surf = list(x = x.pred, y = y.pred, z = z.pred,  
+                                                    facets = NA, fit = fitpoints))
+      
+    }
+  }
+}
---
+++ code_r/meta3.R
@@ -0,0 +1,62 @@
+meta3 <- function(){
+  
+  
+  f1 = "../data/dataD3.in"
+  f2 = "../data/dataD3_avg.in"
+  f3 = "../data/dataD3_2.in"
+  f4 = "../data/dataD3_2_avg.in"
+  f5 = "../data/dataD3_3_Dinic.in"
+  f6 = "../data/dataD3_3_MPM.in"
+  f7 = "../data/dataD3_3_EK.in"
+  
+  #Para provar primeira hipotese ("MPM’s and Dinic’s times, in relation to the number of vertices, follow the same distribution.") usamos uma
+  #MANOVA
+  #df= read.table(f1, header=TRUE)
+  
+  #manova.out <- aov(time ~ as.factor(algorithm) + Error(group), data = df)
+  #summary(manova.out)
+  
+  #Verficamos as assunções
+  #plot(manova.out) não funciona logo fomos ver o qq plot
+  #qqnorm(residuals(manova.out$Within))
+  #qqline(residuals(manova.out$Within))
+  
+  #df2 = read.table(f2, header=TRUE)
+  #As assunções falham logo usaremos kruskal wallis
+  #kruskal.test(time ~ as.factor(algorithm), data=df2)
+  
+  
+  
+  
+  #Para provar segunda hipotese ("All the algorithms’ times, in relation to the connection probability, follow the same distribution.") usamos
+  #uma MANOVA
+  
+  #df3= read.table(f3, header=TRUE)
+  
+  #manova2.out <- aov(time ~ as.factor(algorithm) + Error(group), data = df3)
+  #summary(manova2.out)
+  
+  #Verficamos as assunções
+  #plot(manova2.out) 
+  #qqnorm(residuals(manova2.out$Within))
+  #qqline(residuals(manova2.out$Within))
+  
+  #As assunções falham logo usaremos kruskal wallis
+  #df4 = read.table(f4, header=TRUE)
+  #kruskal.test(time ~ as.factor(algorithm), data=df4)
+  #pairwise.wilcox.test(df4$time, g = df4$algorithm, p.adjust.method="holm")
+  
+  
+  
+  #Para provar a terceira hipotese ("The arc capacity affects the algorithms’ performance.") usamos uma one way anova
+  df5 = read.table(f5, header=TRUE)
+  df6 = read.table(f6, header=TRUE)
+  df7 = read.table(f7, header=TRUE)
+  
+  aov_dinic.out <- aov(time ~ cap , data = df5)
+  aov_mpm.out <- aov(time ~ cap , data = df6)
+  aov_ek.out <- aov(time ~ cap , data = df7)
+  
+  print(summary(aov_dinic.out))
+  plot(aov_dinic.out)
+}
